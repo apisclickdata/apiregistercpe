e7b3175454aae15dba2e661c00708932375e9ca1
Api Pressto <adm@jadalfe.pe>
2023-04-21 18:59:32 -0500

    probar

--- eclickdata/settings.py
+++ eclickdata/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b)r(lxyc$%_392pu4so=24teo#+45-rev3742fnndipyrmhvx4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
